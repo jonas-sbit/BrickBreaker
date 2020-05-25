bd47a5a6748541a3e46d796402d90aa88d04cbf1
Jonas <52232172+jonas-sbit@users.noreply.github.com>
2020-05-25 17:59:16 +0200

    Add files via upload

---
+++ concatenation.py
@@ -0,0 +1,353 @@
+import pygame
+import pygame.freetype
+from pygame.sprite import Sprite
+from pygame.rect import Rect
+from enum import Enum
+from pygame.sprite import RenderUpdates
+from pygame.locals import *
+import sys
+
+BLUE = (106, 159, 181)
+WHITE = (255, 255, 255)
+
+
+def create_surface_with_text(text, font_size, text_rgb, bg_rgb):
+    """ Returns surface with text written on """
+    font = pygame.freetype.SysFont("Courier", font_size, bold=True)
+    surface, _ = font.render(text=text, fgcolor=text_rgb, bgcolor=bg_rgb)
+    return surface.convert_alpha()
+
+
+class UIElement(Sprite):
+    """ An user interface element that can be added to a surface """
+
+    def __init__(self, center_position, text, font_size, bg_rgb, text_rgb, action=None):
+        """
+        Args:
+            center_position - tuple (x, y)
+            text - string of text to write
+            font_size - int
+            bg_rgb (background colour) - tuple (r, g, b)
+            text_rgb (text colour) - tuple (r, g, b)
+            action - the gamestate change associated with this button
+        """
+        self.mouse_over = False
+
+        default_image = create_surface_with_text(
+            text=text, font_size=font_size, text_rgb=text_rgb, bg_rgb=bg_rgb
+        )
+
+        highlighted_image = create_surface_with_text(
+            text=text, font_size=font_size * 1.2, text_rgb=text_rgb, bg_rgb=bg_rgb
+        )
+
+        self.images = [default_image, highlighted_image]
+
+        self.rects = [
+            default_image.get_rect(center=center_position),
+            highlighted_image.get_rect(center=center_position),
+        ]
+
+        self.action = action
+
+        super().__init__()
+
+    @property
+    def image(self):
+        return self.images[1] if self.mouse_over else self.images[0]
+
+    @property
+    def rect(self):
+        return self.rects[1] if self.mouse_over else self.rects[0]
+
+    def update(self, mouse_pos, mouse_up):
+        """ Updates the mouse_over variable and returns the button's
+            action value when clicked.
+        """
+        if self.rect.collidepoint(mouse_pos):
+            self.mouse_over = True
+            if mouse_up:
+                return self.action
+        else:
+            self.mouse_over = False
+
+    def draw(self, surface):
+        """ Draws element onto a surface """
+        surface.blit(self.image, self.rect)
+
+
+class Player:
+    """ Stores information about a player """
+
+    def __init__(self, score=0, lives=3, current_level=1):
+        self.score = score
+        # TODO Leben System evtl Einbauen --> 0 Leben --> zurueck zu Level 1 --> Highscore = Level
+        self.lives = lives
+        self.current_level = current_level
+
+class Breakout:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((800, 600))
+        self.blocks = []
+        self.paddle = [[pygame.Rect(300, 500, 20, 10), 120],
+                [pygame.Rect(320, 500, 20, 10),100],
+                [pygame.Rect(340, 500, 20, 10),80],
+                [pygame.Rect(360, 500, 20, 10),45],
+        ]
+        self.ball = pygame.Rect(300, 490, 5, 5)
+        self.direction = -1
+        self.yDirection = -1
+        self.angle = 80
+
+        # TODO Dynamisch fuer Schwierigkeit
+        self.speeds = {
+            120:(-10, -3),
+            100:(-10, -8),
+            80:(10, -8),
+            45:(10, -3),
+        }
+        self.swap = {
+            120:45,
+            45:120,
+            100:80,
+            80:100,
+        }
+        pygame.font.init()
+        self.font = pygame.font.SysFont("Arial", 25)
+        # TODO self.score umbauen auf Player.score
+        self.score = 0
+
+    def createBlocks(self):
+        # TODO Umbauen fuer Levelsystem
+        self.blocks = []
+        y = 50
+        for __ in range(20):
+            x = 50
+            for _ in range(26):
+                block = pygame.Rect(x, y, 25, 10)
+                self.blocks.append(block)
+                x += 27
+            y += 12
+
+    def ballUpdate(self):
+        for _ in range(2):
+            speed = self.speeds[self.angle]
+            xMovement = True
+            if _:
+                self.ball.x += speed[0] * self.direction
+            else:
+                self.ball.y += speed[1] * self.direction * self.yDirection
+                xMovement = False
+            if self.ball.x <= 0 or self.ball.x >= 800:
+                self.angle = self.swap[self.angle]
+                if self.ball.x <= 0:
+                    self.ball.x = 1
+                else:
+                    self.ball.x = 799
+            if self.ball.y <= 0:
+                self.ball.y = 1
+                self.yDirection *= -1
+            
+            for paddle in self.paddle:
+                if paddle[0].colliderect(self.ball):
+                    self.angle = paddle[1]
+                    self.direction = -1
+                    self.yDirection = -1
+                    break
+            check = self.ball.collidelist(self.blocks)
+            if check != -1:
+                block = self.blocks.pop(check)
+                if xMovement:
+                    self.direction *= -1
+                self.yDirection *= -1
+                self.score += 1
+            if self.ball.y > 600:
+                self.createBlocks()
+                self.score = 0
+                self.ball.x = self.paddle[1][0].x
+                self.ball.y = 490
+                self.yDirection = self.direction = -1
+                
+    def paddleUpdate(self):
+        # TODO Umbauen auf Einstellbar
+        pos = pygame.mouse.get_pos()
+        on = 0
+        for p in self.paddle:
+            p[0].x = pos[0] + 20 * on
+            on += 1
+
+    def main(self, buttons):
+        pygame.mouse.set_visible(False)
+        clock = pygame.time.Clock()
+        self.createBlocks()
+        while True:
+            mouse_up = False
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                    mouse_up = True
+
+            clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+            self.screen.fill(BLUE)
+            self.paddleUpdate()
+            self.ballUpdate()
+
+            for block in self.blocks:
+                pygame.draw.rect(self.screen, (255,255,255), block)
+            for paddle in self.paddle:
+                pygame.draw.rect(self.screen, (255,255,255), paddle[0])
+            pygame.draw.rect(self.screen, (255,255,255), self.ball)
+            self.screen.blit(self.font.render(str(self.score), -1, (255,255,255)), (400, 550))
+            
+            for button in buttons:
+                ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
+                if ui_action is not None:
+                    if ui_action.value == -1:
+                        pygame.quit()
+                        return
+                    
+                    if ui_action.value == 0:
+                        title_screen(pygame.display.set_mode((1000, 750)))
+                    
+                    return ui_action
+
+            buttons.draw(self.screen)
+
+            pygame.display.update()
+
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((1000, 750))
+    game_state = GameState.TITLE
+
+    player = Player()
+
+    while True:
+        if game_state == GameState.TITLE:
+            game_state = title_screen(screen)
+
+        if game_state == GameState.NEWGAME:
+            player = Player()
+            game_state = play_level(screen, player)
+
+        if game_state == GameState.NEXT_LEVEL:
+            player.current_level += 1
+            game_state = play_level(screen, player)
+
+        if game_state == GameState.HIGHSCORE:
+            game_state = highscore_page(screen, player)
+
+        if game_state == GameState.QUIT:
+            pygame.quit()
+            return
+
+
+def title_screen(screen):
+    start_btn = UIElement(
+        center_position=(500, 300),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Start",
+        action=GameState.NEWGAME,
+    )
+    highscore_btn = UIElement(
+        center_position=(500, 400),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Highscores",
+        action=GameState.HIGHSCORE,
+    )
+    quit_btn = UIElement(
+        center_position=(500, 500),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Quit",
+        action=GameState.QUIT,
+    )
+
+    buttons = RenderUpdates(start_btn, quit_btn, highscore_btn)
+
+    return game_loop(screen, buttons, 0)
+
+
+def play_level(screen, player):
+    return_btn = UIElement(
+        center_position=(140, 570),
+        font_size=20,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Return to main menu",
+        action=GameState.TITLE,
+    )
+
+    nextlevel_btn = UIElement(
+        center_position=(400, 400),
+        font_size=30,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text=f"Next level ({player.current_level + 1})",
+        action=GameState.NEXT_LEVEL,
+    )
+
+    buttons = RenderUpdates(return_btn, nextlevel_btn)
+
+    return game_loop(screen, buttons, 1)
+
+
+def highscore_page(screen, player):
+    return_btn = UIElement(
+        center_position=(140, 570),
+        font_size=20,
+        bg_rgb=BLUE,
+        text_rgb=WHITE,
+        text="Return to main menu",
+        action=GameState.TITLE,
+    )
+
+    buttons = RenderUpdates(return_btn)
+
+    return game_loop(screen, buttons, 0)
+
+
+def game_loop(screen, buttons, level):
+    """ Handles game loop until an action is return by a button in the
+        buttons sprite renderer.
+    """
+    while True:
+        mouse_up = False
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+                mouse_up = True
+        screen.fill(BLUE)
+
+        for button in buttons:
+            ui_action = button.update(pygame.mouse.get_pos(), mouse_up)
+            if ui_action is not None:
+                return ui_action
+
+        buttons.draw(screen)
+
+        if level == 1:
+            Breakout().main(buttons)
+
+        pygame.display.flip()
+
+
+class GameState(Enum):
+    QUIT = -1
+    TITLE = 0
+    NEWGAME = 1
+    NEXT_LEVEL = 2
+    HIGHSCORE = 3
+
+
+if __name__ == "__main__":
+    main()
